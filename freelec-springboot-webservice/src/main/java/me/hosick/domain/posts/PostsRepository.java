f4e782463cbcff2f79ed02fec06840ff1d797813
Hosick <ghtjr5000@naver.com>
2019-12-23 21:08:21 +0900

    20191223

---
+++ freelec-springboot-webservice/src/main/java/me/hosick/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package me.hosick.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ freelec-springboot-webservice/src/main/java/me/hosick/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package me.hosick.web.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
