159f8566efa78a2e43710b70b1d627b846a070c8
Hosick <ghtjr5000@naver.com>
2019-12-26 19:50:48 +0900

    20191226

--- freelec-springboot-webservice/src/main/java/me/hosick/service/posts/PostsService.java
+++ freelec-springboot-webservice/src/main/java/me/hosick/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package me.hosick.service.posts;
 
 import lombok.RequiredArgsConstructor;
+import me.hosick.domain.posts.Posts;
 import me.hosick.domain.posts.PostsRepository;
+import me.hosick.web.dto.PostsResponseDto;
 import me.hosick.web.dto.PostsSaveRequestDto;
+import me.hosick.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +14,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    //  포스트 저장
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    // 포스트 수정
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
---
+++ freelec-springboot-webservice/src/main/java/me/hosick/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package me.hosick.web.dto;
+
+import lombok.Getter;
+import me.hosick.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
