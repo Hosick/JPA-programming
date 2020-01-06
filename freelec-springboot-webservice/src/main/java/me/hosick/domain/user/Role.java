ed2be1f0e60b5c2d5ccd9ada53590139af3dec7e
Hosick <ghtjr5000@naver.com>
2020-01-06 22:32:50 +0900

    20200106

--- freelec-springboot-webservice/src/main/java/me/hosick/domain/user/Role.java
+++ freelec-springboot-webservice/src/main/java/me/hosick/domain/user/Role.java
@@ -10,8 +10,7 @@ public enum Role {
     GUEST("ROLE_GUEST", "손님"),
     USER("ROLE_USER", "일반 사용자");
 
-    private final String Key;
+    private final String key;
     private final String title;
-
 
 }
